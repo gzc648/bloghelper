8b7b001e312fa17f25c0105b4095938dff435cb7
英伦之风 <gzcrtw@gmail.com>
2024-11-25 17:00:06 +0800

    Update visit.js

--- visit.js
+++ visit.js
@@ -12,39 +12,42 @@ function getInitialDelay() {
 
 const targetPages = [
     {
-        url: 'https://www.gzcrtw.com/article/financial%20market',
-        searchQuery: 'financial market gzcrtw Blog'
+        url: 'https://www.gzcrtw.com/article/%E6%89%B9%E5%88%A4%E6%80%A7%E6%80%9D%E7%BB%B4%E5%92%8C%E5%85%83%E8%AE%A4%E7%9F%A5%E7%9A%84%E5%85%B3%E7%B3%BB%E4%B8%8E%E5%8C%BA%E5%88%AB',
+        searchQuery: '批判性思维和元认知的关系与区别 gzcrtw Blog'
     },
     {
-        url: 'https://www.gzcrtw.com/article/Outline%20of%20the%20history%20of%20science%20and%20technology',
-        searchQuery: 'Outline of the history of science and technology gzcrtw Blog'
+        url: 'https://www.gzcrtw.com/article/%E9%87%91%E8%9E%8D%E5%B8%82%E5%9C%BA',
+        searchQuery: '金融市场 gzcrtw Blog'
     },
     {
-        url: 'https://www.gzcrtw.com/article/Energetic%20management',
-        searchQuery: 'Energetic management gzcrtw Blog'
+        url: 'https://www.gzcrtw.com/article/%E7%A7%91%E5%AD%A6%E5%8F%B2%E7%BA%B2',
+        searchQuery: '科学史纲 gzcrtw Blog'
     },
     {
-        url: 'https://www.gzcrtw.com/article/Notes%20(unclassified)',
-        searchQuery: 'Notes (unclassified) gzcrtw Blog'
+        url: 'https://www.gzcrtw.com/article/%E7%B2%BE%E5%8A%9B%E7%AE%A1%E7%90%86',
+        searchQuery: '精力管理 gzcrtw Blog'
     },
     {
-        url: 'https://www.gzcrtw.com/article/Negotiation%20Basics',
-        searchQuery: 'clinical depression gzcrtw Blog'
+        url: 'https://www.gzcrtw.com/article/%E7%AC%94%E8%AE%B0%20(%E6%9C%AA%E5%88%86%E7%B1%BB)',
+        searchQuery: '笔记 (未分类) gzcrtw Blog'
     },
     {
-        url: 'https://www.gzcrtw.com/article/cognitive%20development',
-        searchQuery: 'cognitive development gzcrtw Blog'
+        url: 'https://www.gzcrtw.com/article/%E6%8A%91%E9%83%81%E7%97%87',
+        searchQuery: '抑郁症 gzcrtw Blog'
+    },
+    {
+        url: 'https://www.gzcrtw.com/article/%E8%AE%A4%E7%9F%A5%E8%A7%89%E9%86%92',
+        searchQuery: '认知觉醒 gzcrtw Blog'
     },
     {
         url: 'https://gzcrtw.com/',
         searchQuery: 'gzcrtw Blog'
     },
     {
-        url: 'https://www.gzcrtw.com/article/Negotiation%20Basics',
-        searchQuery: 'Negotiation Basics gzcrtw Blog'
+        url: 'https://www.gzcrtw.com/article/%E8%B0%88%E5%88%A4%E5%9F%BA%E7%A1%80',
+        searchQuery: '谈判基础 gzcrtw Blog'
     }
 ];
-
 const MAX_TOTAL_TIME = 840000;
 const randomWait = () => Math.floor(Math.random() * (300 - 10 + 1) + 10) * 1000;
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
